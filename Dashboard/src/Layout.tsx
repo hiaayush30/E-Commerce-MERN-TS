e9d689b27c10124824f0cdcfb436e33ca27bd798
aayush <hiaayush30@gmail.com>
2025-02-11 13:31:13 +0530

    updated sidebar ui

--- Dashboard/src/Layout.tsx
+++ Dashboard/src/Layout.tsx
@@ -3,7 +3,7 @@ import AdminSideBar from "./components/AdminSideBar"
 
 const Layout = () => {
     return (
-        <div className="grid grid-cols-[1fr_4fr] grid-rows-1 overflow-y-auto scrollbar-hide bg-[#B6C7AA] text-slate-800 min-h-screen">
+        <div className="grid grid-cols-[1fr_4fr] grid-rows-1 overflow-y-auto scrollbar-hide bg-[#C4D9FF] text-slate-800 min-h-screen">
             <AdminSideBar />
             <Outlet />
         </div>
--- Dashboard/src/components/AdminSideBar.tsx
+++ Dashboard/src/components/AdminSideBar.tsx
@@ -1,86 +1,67 @@
 import { MdDashboard } from "react-icons/md";
 import { FaChartLine, FaChartPie, FaShoppingBag } from "react-icons/fa";
 import { IoIosPeople } from "react-icons/io";
-import { NavLink } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { LuNotebookPen } from "react-icons/lu";
 import { FaChartBar } from "react-icons/fa";
 import { RiCoupon2Fill } from "react-icons/ri";
 import { FaStopwatch } from "react-icons/fa6";
 import { RiCoinFill } from "react-icons/ri";
+import { IconType } from "react-icons";
 
 const AdminSideBar = () => {
   return (
-    <aside className="w-full flex flex-col bg-[#E7D4B5]">
+    <aside className="w-full flex flex-col bg-[#E8F9FF]">
       <h2 className="text-3xl p-2 font-semibold">Logo.</h2>
       <div className="ml-5 mr-3">
         <h3 className="text-lg">DASHBOARD</h3>
         <div className="mr-2">
-          <NavLink className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-            to={'/admin/dashboard'}>
-            <MdDashboard />
-            <span>Dashboard</span>
-          </NavLink>
-          <NavLink className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-            to={'/admin/products'}>
-            <FaShoppingBag />
-            <span>Products</span>
-          </NavLink>
-          <NavLink className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-            to={'/admin/customers'}>
-            <IoIosPeople />
-            <span>Customers</span>
-          </NavLink>
-          <NavLink className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-            to={'/admin/transactions'}>
-            <LuNotebookPen />
-            <span>Transaction</span>
-          </NavLink>
+          <SidebarOptions icon={MdDashboard} text="Dashboard" url="/admin/dashboard" />
+          <SidebarOptions icon={FaShoppingBag} text="Products" url="/admin/products" />
+          <SidebarOptions icon={IoIosPeople} text="Customers" url="/admin/customers" />
+          <SidebarOptions icon={LuNotebookPen} text="Transactions" url="/admin/transactions" />
         </div>
         <h3>CHARTS</h3>
         <div>
-          <NavLink to={'/admin/bar'}
-            className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-          >
-            <FaChartBar />
-            <span>Bar</span>
-          </NavLink>
-          <NavLink to={'/admin/pie'}
-            className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-          >
-            <FaChartPie />
-            <span>Pie</span>
-          </NavLink>
-          <NavLink to={'/admin/line'}
-            className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-          >
-            <FaChartLine />
-            <span>Line</span>
-          </NavLink>
+          <SidebarOptions icon={FaChartBar} text="Bar" url="/admin/bar" />
+          <SidebarOptions icon={FaChartPie} text="Pie" url="/admin/pie" />
+          <SidebarOptions icon={FaChartLine} text="Line" url="/admin/line" />
         </div>
         <h3>APPS</h3>
         <div>
-          <NavLink to={'/admin/stopwatch'}
-            className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-          >
-            <FaStopwatch />
-            <span>Stopwatch</span>
-          </NavLink>
-          <NavLink to={'/admin/coupon'}
-            className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-          >
-            <RiCoupon2Fill />
-            <span>Coupon</span>
-          </NavLink>
-          <NavLink to={'/admin/toss'}
-            className={({ isActive }) => `${isActive ? 'bg-[#B6C7AA] text-[#556d2a]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
-          >
-            <RiCoinFill />
-            <span>Toss</span>
-          </NavLink>
+          <SidebarOptions icon={FaStopwatch} text="Stopwatch" url="/admin/stopwatch" />
+          <SidebarOptions icon={RiCoupon2Fill} text="Coupon" url="/admin/coupon" />
+          <SidebarOptions icon={RiCoinFill} text="Toss" url="/admin/toss" />
         </div>
       </div>
     </aside>
   )
 }
 
+interface OptionsProps {
+  url: string;
+  text: string;
+  icon: IconType;
+}
+// const SidebarOptions = (props: OptionsProps) => {
+//   return <NavLink to={props.url}
+//     className={({ isActive }) => `${isActive ? 'bg-[#7E60BF] text-[#FFE1FF]' : ''} flex items-center gap-2 rounded-md p-1 m-1`}
+//   >
+//     <props.icon />
+//     <span>{props.text}</span>
+//   </NavLink>
+// }
+
+//or
+const SidebarOptions = (props: OptionsProps) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return <li
+    onClick={()=>navigate(props.url)}
+    className={`${location.pathname == props.url ? 'bg-[#7E60BF] text-[#FFE1FF]' : 'hover:bg-[#D6CCF7]'} flex items-center gap-2 rounded-md p-1 m-1 cursor-pointer`}
+  >
+    <props.icon />
+    <span>{props.text}</span>
+  </li>
+}
 export default AdminSideBar

0094bce72e2d6b0d1c1b44901b49306f258db8cd
aayush <hiaayush30@gmail.com>
2025-03-05 00:46:20 +0530

    added signup route

--- Server/src/controllers/auth.controller.ts
+++ Server/src/controllers/auth.controller.ts
@@ -1,10 +1,63 @@
 import { Request, Response } from "express";
+import zod from 'zod';
+import User from "../models/user.model";
+import jwt from 'jsonwebtoken';
 
-export const handleLogin =async (req:Request,res:Response) => {
-    res.send('login route called');
+const SignupSchema = zod.object({
+    name:zod.string(),
+    email:zod.string().email(),
+    password:zod.string()
+})
+export const handleSignup =async (req:Request,res:Response):Promise<any> => {
+    try {
+        const validRequest = SignupSchema.safeParse(req.body);
+        if(!validRequest.success){
+            return res.status(400).json({
+                message:"invalid request",
+                error:validRequest.error.format()
+            })
+        }
+        const {email,password,name} = req.body;
+        const existingUser = await User.findOne({
+            email,
+        })
+        if(existingUser){
+            return res.status(403).json({
+                message:"email already exists"
+            })
+        }
+        const user = await User.create({
+            name,
+            password,
+            email
+        });
+        const token = jwt.sign({
+            id:user.id,
+            email:user.email
+        },process.env.JWT_PASS as string,{
+            expiresIn:'30d'
+        });
+        res.cookie('token',token,{
+            maxAge:30*2*60*60*1000,
+            httpOnly:true,
+            secure:process.env.ENVIRONMENT!="development",
+            sameSite:"none"
+        })
+        return res.status(201).json({
+            message:"user signed up successfully",
+            user:{
+                "name":user.name,
+                "email":user.email,
+                "id":user._id,
+                "cartItems":user.cartItems
+            }
+        }) 
+    } catch (error) {
+        console.log('Error in signup: '+error);
+    }
 }
 
-export const handleSignup =async (req:Request,res:Response) => {
+export const handleLogin =async (req:Request,res:Response) => {
     res.send('signup route called');
 }
 
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -4,22 +4,24 @@ import express from 'express';
 import cors from 'cors';
 import { connectDatabase } from './config/db';
 import { authRouter } from './routes/auth.route';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     origin: process.env.BE_DOMAIN,
     credentials: true
 }))
 
-app.get('/health', (req, res) => {
+app.get('/api/health', (req, res) => {
     res.json({
         message: "server running"
     })
 })
 
-app.use('/auth',authRouter);
+app.use('/api/auth',authRouter);
 
 connectDatabase()
     .then(() => {
